0b127f6cc9c54e3268432763b7fe38626b7354ef
WardenQQ <WardenQQ@server.fake>
2014-05-06 11:45:31 +0200

    Ajout de la classe de base LieuOrigine

---
+++ src/gestionBiere/LieuOrigine.java
@@ -0,0 +1,29 @@
+package gestionBiere;
+
+import java.util.UUID;
+
+public class LieuOrigine {
+    private final String identifiant;
+    private String nom;
+    private String paysAppartenance;
+
+    public LieuOrigine() {
+        identifiant = UUID.randomUUID().toString();
+    }
+
+    public LieuOrigine(String nom, String paysAppartenance) {
+        this();
+        setNom(nom);
+        setPaysAppartenance(paysAppartenance);
+    }
+
+    public String getIdentifiant() { return identifiant; }
+
+    public String getNom() { return nom; }
+    public void setNom(String nom) { this.nom = nom; }
+
+    public String getPaysAppartenance() { return paysAppartenance; }
+    public void setPaysAppartenance(String paysAppartenance) {
+        this.paysAppartenance = paysAppartenance;
+    }
+}
