4b8fc116db329c6bf55d9d9ad99e895b34e9e127
WardenQQ <WardenQQ@server.fake>
2014-05-10 12:32:54 +0200

    Simplification de la classe Bouteille.

--- src/gestionBiere/Bouteille.java
+++ src/gestionBiere/Bouteille.java
@@ -3,99 +3,85 @@ package gestionBiere;
 import java.io.Serializable;
 
 public class Bouteille implements Serializable {
-	private static int compteur = 1;
+    private static int compteur = 1;
 
-	private final int idBouteille;
+    private final int identifiant;
 
-	private int taille = 0;
-	private String type = ""; 
-	private String bouchon = "";
-	private String lienPhoto = ""; 
-	private Breuvage breuvage = new Breuvage(); 
-	
-	public Bouteille()
-	{
-		this.idBouteille = compteur;
-		compteur++;
-		this.breuvage = new Breuvage();
-	}
-	
-	public Bouteille(int taille,String type,String bouchon,String lienPhoto,Breuvage breuvage)
-	{
-		this();
-		setBouchon(bouchon);
-		setBreuvage(breuvage);
-		setType(type);
-		setTailleBouteille(taille);
-		setLienPhoto(lienPhoto);
-	}
+    private int taille = 0;
+    private String type = ""; 
+    private String bouchon = "";
+    private String lienPhoto = ""; 
+    private Breuvage breuvage = new Breuvage(); 
 
-        public boolean equals(Object obj) {
-            boolean isEqual = false;
+    public Bouteille() {
+        this.identifiant = compteur;
+        compteur++;
+    }
 
-            if (obj instanceof Bouteille) {
-                Bouteille bouteille = (Bouteille)obj;
-                isEqual = this.taille == bouteille.taille &&
-                          this.type.equals(bouteille.type) &&
-                          this.bouchon.equals(bouteille.bouchon) &&
-                          this.breuvage.equals(bouteille.breuvage);
-            }
+    public Bouteille(int taille, String type, String bouchon, String lienPhoto, Breuvage breuvage) {
+        this();
+        setBouchon(bouchon);
+        setBreuvage(breuvage);
+        setType(type);
+        setTailleBouteille(taille);
+        setLienPhoto(lienPhoto);
+    }
 
-            return isEqual;
+    public boolean equals(Object obj) {
+        boolean isEqual = false;
+
+        if (obj instanceof Bouteille) {
+            Bouteille bouteille = (Bouteille)obj;
+            isEqual = this.taille == bouteille.taille &&
+                this.type.equals(bouteille.type) &&
+                this.bouchon.equals(bouteille.bouchon) &&
+                this.breuvage.equals(bouteille.breuvage);
         }
-	
-	public void setBouchon(String bouchon)
-	{
-		this.bouchon = bouchon;
-	}
-	
-	public void setBreuvage(Breuvage breuvage)
-	{
-		this.breuvage = breuvage;
-	}
-	
-	public void setType(String type)
-	{
-		this.type = type;
-	}
-	
-	public void setTailleBouteille(int taille)
-	{
-		this.taille = taille;
-	}
-	
-	public void setLienPhoto(String lienPhoto)
-	{
-		this.lienPhoto = lienPhoto;
-	}
-	
-	public String getBouchon()
-	{
-		return this.bouchon;
-	}
-	
-	public int getTaille()
-	{
-		return this.taille;
-	}
-	
-	public String getType()
-	{
-		return this.type;
-	}
-	
-	public String getLienPhoto()
-	{
-		return this.lienPhoto;
-	}
-	
-	public Breuvage getBreuvage()
-	{
-		return this.breuvage;
-	}
-	
-	public int getIdBouteille()
-	{
-		return this.idBouteille;
-	}
+
+        return isEqual;
+    }
+
+    public void setBouchon(String bouchon) {
+        this.bouchon = bouchon;
+    }
+
+    public void setBreuvage(Breuvage breuvage) {
+        this.breuvage = breuvage;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setTailleBouteille(int taille) {
+        this.taille = taille;
+    }
+
+    public void setLienPhoto(String lienPhoto) {
+        this.lienPhoto = lienPhoto;
+    }
+
+    public String getBouchon() {
+        return this.bouchon;
+    }
+
+    public int getTaille() {
+        return this.taille;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public String getLienPhoto() {
+        return this.lienPhoto;
+    }
+
+    public Breuvage getBreuvage() {
+        return this.breuvage;
+    }
+
+    public int getIdentifiant() {
+        return this.identifiant;
+    }
 }
