b05524343f429a8eed6c9596811a5ca629857dd4
WardenQQ <WardenQQ@server.fake>
2014-05-11 13:37:28 +0200

    Ajout du modèle Lieu Origine

---
+++ src/modele/LieuOrigineModele.java
@@ -0,0 +1,44 @@
+package modele;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import gestionBiere.LieuOrigine;
+
+public class LieuOrigineModele extends AbstractTableModel 
+{
+    private static final long serialVersionUID = 1L;
+    private final String[] entete = { "Identifiant", "Ville d'origine", "Pays d'origine", };
+    private ArrayList<LieuOrigine> listeLieuOrigine;
+
+    public LieuOrigineModele(ArrayList<LieuOrigine> listeLieuOrigine) {
+        super();
+        this.listeLieuOrigine = listeLieuOrigine;
+    }
+
+    public int getRowCount() {
+        return listeLieuOrigine.size();
+    }
+
+    public int getColumnCount() {
+        return entete.length;
+    }
+
+    public String getColumnName(int columnIndex) {
+        return entete[columnIndex];
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return listeLieuOrigine.get(rowIndex).getIdentifiant();
+            case 1:
+                return listeLieuOrigine.get(rowIndex).getNom();
+            case 2:
+                return listeLieuOrigine.get(rowIndex).getPaysAppartenance();
+            default:
+                return null;
+        }
+    }
+}
