a2ccf76bacba3b67756ba9d97ffdbb14ace16f0d
Frank <frank@localhost.localdomain>
2014-05-11 11:04:50 +0200

    Ajout d'interaction menu principal

--- src/BeerProject.java
+++ src/BeerProject.java
@@ -1,19 +1,7 @@
 import Graphics.FenetrePrincipale;
-import bdd.*;
-import gestionBiere.*;
-import java.util.ArrayList;
 
 public class BeerProject {
     public static void main(String[] args) {
-        //FenetrePrincipale p = new FenetrePrincipale();
-
-        BaseDonnee bdd = new BaseDonnee("breuvage.bdd");
-
-        ArrayList<StringBuffer> liste = bdd.getListeCouleur();
-        StringBuffer str = liste.get(0);
-        str.replace(0, str.length(), "Fucking Bill Burr");
-
-        ArrayList<Breuvage> test = bdd.getListeBreuvage();
-        System.out.println(test.get(0).getCouleur());
+        new FenetrePrincipale();
     }
 }
--- src/Graphics/FenetrePrincipale.java
+++ src/Graphics/FenetrePrincipale.java
@@ -3,12 +3,15 @@ package Graphics;
 import gestionBiere.*;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.CardLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -18,33 +21,40 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
 
 import bdd.BaseDonnee;
 
 public class FenetrePrincipale extends JFrame
 {
 	private static final long serialVersionUID = 1L;
+	private JPanel interfaceGraphique = new JPanel();
 	private JPanel menu = new JPanel();
+	private JPanel menuAjout = new JPanel();
+	private JPanel menuSupr = new JPanel();
 	private JTable affichage = new JTable();
-	private JPanel menuLateral = new JPanel();
-	private JPanel barreRecherche = new JPanel();
-	private JTextField champRecherche = new JTextField("Tapez ici votre recherche");
 	private String[] nomBoutons = {"Ajout","Suppression","Quitter"};
 	private String[] choixMenu = {"Breuvage","Bouteille","Brasserie","Lieu origine","Commentaire"};
-	private String[] entete = {"Id", "Nom", "Nom Brasserie", "Couleur"
+	private String[] enteteBreuvage = {"Id", "Nom", "Nom Brasserie", "Couleur"
 			,"Fermentation","Provenance","Lieu d'origine","Pays d'origine"}; 
-	private JButton[] tabBoutons = new JButton[nomBoutons.length];
+	private String[] enteteBrasserie = {"Id","Nom","Nom Brasserie"};
+	private Dimension dimFormulaire = new Dimension(400,500);
 	private Dimension dimBouton = new Dimension(130,40);
-	private Dimension dimTab = new Dimension(900,500);
-	private Dimension dimPanMenuLateral = new Dimension(140,500);
+	private Dimension dimTab = new Dimension(900,600);
+	private Dimension dimPanMenuLateral = new Dimension(140,600);
 	private Dimension dimPanRecherche = new Dimension(900,40);
 	private Dimension dimRecherche = new Dimension(250,30);
 	private String nomFichier = "breuvage.bdd";
 	private BaseDonnee bdd = new BaseDonnee(nomFichier);
-	private JComboBox choixListe = initComboBox(choixMenu);
 	private String selectionActuelle = "Breuvage";
-	private JLabel label = new JLabel("Recherche : ");
-	private JButton okButton = new JButton ("OK");
+	private CardLayout layout = new CardLayout();
+	private String[] listePanel = {"Principal", "Ajout", "Modification"};
+	private JComboBox choixListe = initComboBox(choixMenu);
+	private breuvageModele breuvage = new breuvageModele();
+	private brasserieModele brasserie = new brasserieModele();
+	private enum Choix {
+	    Breuvage;
+	}
 	
 	public FenetrePrincipale()
 	{
@@ -55,32 +65,50 @@ public class FenetrePrincipale extends JFrame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		
-		initMenuPrincipal();
+		initInterface();
 		
-		this.setContentPane(menu);
+		this.setContentPane(interfaceGraphique);
 		this.setVisible(true);
 	}
 	
-	private void initMenuPrincipal()
+	private void initInterface()
 	{
-		ArrayList<Breuvage> breuvage = bdd.getListeBreuvage();
+		initPagePrincipale();
+		initPageAjout();
+		interfaceGraphique.setLayout(layout);
+		interfaceGraphique.add(menu, listePanel[0]);
+		interfaceGraphique.add(menuAjout, listePanel[1]);
+		interfaceGraphique.add(menuSupr, listePanel[2]);
+	}
+	
+	private void initPagePrincipale()
+	{
+		
+		JPanel menuLateral = new JPanel();
+		JPanel barreRecherche = new JPanel();
+		JLabel label = new JLabel("Recherche : ");
+		JButton okButton = new JButton ("OK");
+		JButton[] tabBoutons = new JButton[nomBoutons.length];
+		JTextField champRecherche = new JTextField("Tapez ici votre recherche");
 		
 		menuLateral.setPreferredSize(dimPanMenuLateral);
 		
 		barreRecherche.setPreferredSize(dimPanRecherche);
 		
 		choixListe.setPreferredSize(dimBouton);
-		choixListe.addActionListener(new changementSelectionMenu());
+		choixListe.addActionListener(new choixAffichage());
 		
-		affichage = new JTable(breuvageTab(breuvage), entete);
+		affichage = new JTable(this.breuvage);
+		affichage.setAutoCreateRowSorter(true);
 		affichage.setPreferredSize(dimTab);
 		
 		champRecherche.setPreferredSize(dimRecherche);
 		
 		label.setPreferredSize(dimBouton);
 		
-		okButton.addActionListener(new BoutonRecherche());
+		okButton.addActionListener(new boutonRech());
 		
+		menuLateral.add(choixListe,BorderLayout.NORTH);
 		for(int i = 0; i < nomBoutons.length ; i++)
 		{
 			tabBoutons[i] = new JButton(nomBoutons[i]);
@@ -88,15 +116,15 @@ public class FenetrePrincipale extends JFrame
 			switch(i)
 			{
 				case 0 :
-					tabBoutons[i].addActionListener(new BoutonAjout());
+					tabBoutons[i].addActionListener(new boutonAjout());
 					menuLateral.add(tabBoutons[i]);
 					break;
 				case 1 :
-					tabBoutons[i].addActionListener(new BoutonDelete());
+					tabBoutons[i].addActionListener(new boutonModif());
 					menuLateral.add(tabBoutons[i]);
 					break;
 				case 2 :
-					tabBoutons[i].addActionListener(new BoutonQuitter());
+					tabBoutons[i].addActionListener(new boutonQuitter());
 					menuLateral.add(tabBoutons[i]);
 					break;
 				default :
@@ -109,10 +137,61 @@ public class FenetrePrincipale extends JFrame
 		barreRecherche.add(label,BorderLayout.WEST);
 		barreRecherche.add(champRecherche,BorderLayout.WEST);
 		barreRecherche.add(okButton,BorderLayout.WEST);
-		menuLateral.add(choixListe,BorderLayout.SOUTH);
+		
 		menu.add(barreRecherche, BorderLayout.NORTH);
-		menu.add(sp,BorderLayout.WEST);
+		menu.add(sp,BorderLayout.SOUTH);
 		menu.add(menuLateral, BorderLayout.EAST);
+	}
+	
+	private void initPageAjout()
+	{
+		JPanel formulaire = new JPanel();
+		JPanel panBouton = new JPanel();
+		
+		formulaire.setPreferredSize(dimFormulaire);
+		panBouton.setPreferredSize(dimPanRecherche);
+		Choix choix = Choix.valueOf(selectionActuelle); // surround with try/catch
+		switch(choix)
+		{
+		case Breuvage :
+			JLabel labelNom = new JLabel("Nom :");
+			JTextField champNom = new JTextField("Nom");
+			JLabel labelTaux = new JLabel("Taux D'alcool : ");
+			JTextField champTaux = new JTextField("TauxAlcool");
+			JLabel labelAnnee = new JLabel("Annee Origine : ");
+			JTextField champAnnee = new JTextField("Annee Origine");
+			//JComboBox choixFermentation = initComboBox((String[]) bdd.getListeFermentation().toArray());
+			//JComboBox choixTypeFermentation = initComboBox((String[]) bdd.getListeTypeFermentation().toArray());
+			//JComboBox choixProvenance = initComboBox((String[])bdd.getListeProvenance().toArray());
+			//JComboBox choixCouleur = initComboBox((String[]) bdd.getListeCouleur().toArray());
+			formulaire.add(labelNom);
+			formulaire.add(champNom);
+			formulaire.add(labelTaux);
+			formulaire.add(champTaux);
+			formulaire.add(labelAnnee);
+			formulaire.add(champAnnee);
+			//formulaire.add(choixFermentation);
+			//formulaire.add(choixTypeFermentation);
+			//formulaire.add(choixProvenance);
+			//formulaire.add(choixCouleur);
+			
+			JButton ajoutButton = new JButton("Ajouter");
+			JButton cancelButton = new JButton("Annuler");
+			cancelButton.addActionListener(new boutonAjoutCancel());
+			ajoutButton.addActionListener(new boutonAjoutOk());
+			panBouton.add(ajoutButton);
+			panBouton.add(cancelButton);
+			break;
+		default :
+			break;
+		}
+		menuAjout.add(panBouton, BorderLayout.NORTH);
+		menuAjout.add(formulaire, BorderLayout.SOUTH);
+	}
+	
+	private void initPageModif()
+	{
+		
 	}
 	
 	private JComboBox initComboBox(String[] choix)
@@ -125,64 +204,261 @@ public class FenetrePrincipale extends JFrame
 		return choixBox;
 	}
 	
-	private Object[][] breuvageTab(ArrayList<Breuvage> aL)
+	class boutonCancel implements ActionListener
 	{
-		Object[][] oL = new Object[aL.size()][8];
-		Iterator<Breuvage> ite = aL.iterator();
-		Breuvage elem = new Breuvage();
-		int i = 0;
-		for(i = 0; ite.hasNext() != false ; i++)
+		public void actionPerformed(ActionEvent e)
 		{
-			elem = ite.next();
-			oL[i][0] = elem.getIdentifiant();
-			oL[i][1] = elem.getNom();
-			oL[i][2] = elem.getBrasserie().getNom();
-			oL[i][3] = elem.getCouleur();
-			oL[i][4] = elem.getFermentation();
-			oL[i][5] = elem.getProvenance();
-			oL[i][6] = elem.getBrasserie().getLieuOrigine().getNom();
-			oL[i][7] = elem.getBrasserie().getLieuOrigine().getPaysAppartenance();
-		}
-		return oL;
-	}
-	
-	class BoutonRecherche implements ActionListener
-	{
-		public void actionPerformed(ActionEvent act)
-		{
-			
+			layout.show(interfaceGraphique, listePanel[0]);
 		}
 	}
 	
-	class BoutonAjout implements ActionListener
+	class boutonQuitter implements ActionListener
 	{
-		public void actionPerformed(ActionEvent act)
-		{
-			
-		}
-	}
-	
-	class BoutonDelete implements ActionListener
-	{
-		public void actionPerformed(ActionEvent act)
-		{
-			
-		}
-	}
-	
-	class BoutonQuitter implements ActionListener
-	{
-		public void actionPerformed(ActionEvent act)
+		public void actionPerformed(ActionEvent e)
 		{
 			dispose();
 		}
 	}
 	
-	class changementSelectionMenu implements ActionListener
+	class boutonModif implements ActionListener
 	{
 		public void actionPerformed(ActionEvent e)
 		{
 			
 		}
+	}
+	
+	class boutonRech implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			dispose();
+		}
+	}
+	
+	class boutonAjoutCancel implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			layout.show(interfaceGraphique, listePanel[0]);
+		}
+	}
+	
+	class boutonAjoutOk implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			if (selectionActuelle.compareTo("Breuvage") == 0)
+			{
+				Breuvage newB = new Breuvage();
+				breuvage.addBreuvage(newB);
+			}
+		}
+	}
+	
+	class boutonAjout implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			layout.show(interfaceGraphique, listePanel[1]);
+		}
+	}
+	
+	class choixAffichage implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e)
+		{
+			selectionActuelle = choixListe.getSelectedItem().toString();
+			if (selectionActuelle.compareTo("Brasserie") == 0)
+			{
+				affichage.setModel(brasserie);
+				affichage.repaint();
+			}
+			else if (selectionActuelle.compareTo("Breuvage") == 0)
+			{
+				affichage.setModel(breuvage);
+				affichage.repaint();
+			}
+			else if (selectionActuelle.compareTo("LieuOrigine") == 0)
+			{
+				
+			}
+		}
+	}
+	
+	class breuvageModele extends AbstractTableModel
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private final List<Breuvage> breuvage = new ArrayList<Breuvage>();
+		
+		public breuvageModele()
+		{
+			super();
+			breuvage.addAll(bdd.getListeBreuvage());
+		}
+		
+		public int getRowCount() 
+		{
+	        return breuvage.size();
+	    }
+		
+		public int getColumnCount()
+		{
+	        return enteteBreuvage.length;
+	    }
+		
+		public String getColumnName(int columnIndex) 
+		{
+	        return enteteBreuvage[columnIndex];
+	    }
+		
+		public Object getValueAt(int rowIndex, int columnIndex) 
+		{
+	        switch(columnIndex){
+	            case 0:
+	                return breuvage.get(rowIndex).getIdentifiant();
+	            case 1:
+	                return breuvage.get(rowIndex).getNom();
+	            case 2:
+	                return breuvage.get(rowIndex).getBrasserie().getNom();
+	            case 3:
+	                return breuvage.get(rowIndex).getCouleur();
+	            case 4:
+	                return breuvage.get(rowIndex).getFermentation();
+	            case 5:
+	                return breuvage.get(rowIndex).getProvenance();
+	            case 6:
+	                return breuvage.get(rowIndex).getLieuOrigine().getNom();
+	            case 7:
+	                return breuvage.get(rowIndex).getLieuOrigine().getPaysAppartenance();
+	            default:
+	                return null; 
+	        }
+	    }
+		public void addBreuvage(Breuvage newB) 
+		{
+	        breuvage.add(newB);
+	 
+	        fireTableRowsInserted(breuvage.size() -1, breuvage.size() -1);
+	    }
+		public void removeAmi(int rowIndex) {
+	        breuvage.remove(rowIndex);
+	 
+	        fireTableRowsDeleted(rowIndex, rowIndex);
+	    }
+	}
+	
+	class brasserieModele extends AbstractTableModel
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private final List<Brasserie> brasserie = new ArrayList<Brasserie>();
+		
+		public brasserieModele()
+		{
+			super();
+			brasserie.addAll(bdd.getListeBrasserie());
+		}
+		
+		public int getRowCount() 
+		{
+	        return brasserie.size();
+	    }
+		
+		public int getColumnCount() 
+		{
+	        return enteteBrasserie.length;
+	    }
+		
+		public String getColumnName(int columnIndex) 
+		{
+	        return enteteBrasserie[columnIndex];
+	    }
+		
+		public Object getValueAt(int rowIndex, int columnIndex) 
+		{
+	        switch(columnIndex){
+	            case 0:
+	                return brasserie.get(rowIndex).getIdentifiant();
+	            case 1:
+	                return brasserie.get(rowIndex).getNom();
+	            case 2:
+	                return brasserie.get(rowIndex).getLieuOrigine().getNom();
+	            case 3:
+	                return brasserie.get(rowIndex).getLieuOrigine().getPaysAppartenance();
+	            default:
+	                return null; 
+	        }
+	    }
+		public void addBrasserie(Brasserie newB) 
+		{
+	        brasserie.add(newB);
+	 
+	        fireTableRowsInserted(brasserie.size() -1, brasserie.size() -1);
+	    }
+		public void removeBrasserie(int rowIndex) {
+	        brasserie.remove(rowIndex);
+	 
+	        fireTableRowsDeleted(rowIndex, rowIndex);
+	    }
+	}
+	
+	class LieuOrigineModele extends AbstractTableModel
+	{
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private final List<LieuOrigine> lieuOrigine = new ArrayList<LieuOrigine>();
+		private String[] enteteLieuOrigine = {"Nom","Pays appartenance"};
+		
+		public LieuOrigineModele()
+		{
+			super();
+			lieuOrigine.addAll(bdd.getListeLieuOrigine());
+		}
+		
+		public int getRowCount() 
+		{
+	        return lieuOrigine.size();
+	    }
+		
+		public int getColumnCount() 
+		{
+	        return enteteLieuOrigine.length;
+	    }
+		
+		public String getColumnName(int columnIndex) 
+		{
+	        return enteteLieuOrigine[columnIndex];
+	    }
+		
+		public Object getValueAt(int rowIndex, int columnIndex) 
+		{
+	        switch(columnIndex){
+	            case 0:
+	                return lieuOrigine.get(rowIndex).getNom();
+	            case 1:
+	                return lieuOrigine.get(rowIndex).getPaysAppartenance();
+	            default:
+	                return null; 
+	        }
+	    }
+		public void addLieuOrigine(LieuOrigine newB) 
+		{
+	        lieuOrigine.add(newB);
+	 
+	        fireTableRowsInserted(lieuOrigine.size() -1, lieuOrigine.size() -1);
+	    }
+		public void removeLieuOrigine(int rowIndex) {
+			lieuOrigine.remove(rowIndex);
+	 
+	        fireTableRowsDeleted(rowIndex, rowIndex);
+	    }
 	}
 }
