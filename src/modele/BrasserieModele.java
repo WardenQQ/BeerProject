eb7fda91581c0baf79da306985c94cc641697377
WardenQQ <WardenQQ@server.fake>
2014-05-11 13:02:02 +0200

    Ajout du modèle de Brasserie.

---
+++ src/modele/BrasserieModele.java
@@ -0,0 +1,46 @@
+package modele;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+import gestionBiere.Brasserie;
+
+public class BrasserieModele extends AbstractTableModel 
+{
+    private static final long serialVersionUID = 1L;
+    private final String[] entete = {"Identifiant", "Nom de la Brasserie", "Ville d'origine", "Pays d'origine"};
+    private ArrayList<Brasserie> listeBrasserie;
+
+    public BrasserieModele(ArrayList<Brasserie> listeBrasserie) {
+        super();
+        this.listeBrasserie = listeBrasserie;
+    }
+
+    public int getRowCount() {
+        return listeBrasserie.size();
+    }
+
+    public int getColumnCount() {
+        return entete.length;
+    }
+
+    public String getColumnName(int columnIndex) {
+        return entete[columnIndex];
+    }
+
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        switch (columnIndex) {
+            case 0:
+                return listeBrasserie.get(rowIndex).getIdentifiant();
+            case 1:
+                return listeBrasserie.get(rowIndex).getNom();
+            case 2:
+                return listeBrasserie.get(rowIndex).getLieuOrigine().getNom();
+            case 3:
+                return listeBrasserie.get(rowIndex).getLieuOrigine().getPaysAppartenance();
+            default:
+                return null;
+        }
+    }
+}
